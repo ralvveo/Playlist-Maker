16fcaf018ea0e2180310aab8f0e1aa15dcfd56a2
ralvveo <lvravdin@edu.hse.ru>
2023-12-22 13:03:14 +0300

    Спринт 9 попытка 1

--- app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
@@ -1,11 +1,89 @@
 package com.practicum.playlistmaker
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import android.widget.ImageView
 
 class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        //Кнопка Назад
+        val searchButtonBack = findViewById<ImageView>(R.id.search_button_back)
+        searchButtonBack.setOnClickListener {
+
+            val displayIntent = Intent(this, MainActivity::class.java)
+            startActivity(displayIntent)
+        }
+
+        //Видимость Крестика в Поиске
+
+        val inputEditText = findViewById<EditText>(R.id.input_edit_text)
+        val clearButton = findViewById<ImageView>(R.id.clear_icon)
+
+        clearButton.setOnClickListener {
+            inputEditText.setText("")
+        }
+
+        fun clearButtonVisibility(s: CharSequence?): Int {
+
+            return if (s.isNullOrEmpty()) {
+                val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+                inputMethodManager?.hideSoftInputFromWindow(clearButton.windowToken, 0)
+                View.GONE
+
+
+            } else {
+                View.VISIBLE
+            }
+        }
+
+        //Text Watcher
+        val simpleTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // empty
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+                clearButton.visibility = clearButtonVisibility(s)
+
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                // empty
+            }
+        }
+        inputEditText.addTextChangedListener(simpleTextWatcher)
+    }
+
+    //Сохранение введенного в Поиске Текста
+    private var searchText = TEXT
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        val inputEditText = findViewById<EditText>(R.id.input_edit_text)
+        searchText = inputEditText.text.toString()
+        outState.putString(SEARCH_TEXT, searchText)
+    }
+
+    companion object {
+        const val SEARCH_TEXT = "SEARCH_TEXT"
+        const val TEXT = ""
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        searchText = savedInstanceState.getString(SEARCH_TEXT, TEXT)
+        val inputEditText = findViewById<EditText>(R.id.input_edit_text)
+        inputEditText.setText(searchText)
     }
 }
--- app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
@@ -1,9 +1,11 @@
 package com.practicum.playlistmaker
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.FrameLayout
 import android.widget.ImageView
 
 class SettingsActivity : AppCompatActivity() {
@@ -11,11 +13,46 @@ class SettingsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
 
+
+        //Кнопка Назад
         val settingsButtonBack = findViewById<ImageView>(R.id.settings_button_back)
         settingsButtonBack.setOnClickListener {
 
             val displayIntent = Intent(this, MainActivity::class.java)
             startActivity(displayIntent)
         }
+
+        //Кнопка Поделиться приложением
+        val settingsButtonShare = findViewById<FrameLayout>(R.id.settings_button_share)
+        settingsButtonShare.setOnClickListener {
+
+            val shareMessage = getString(R.string.share_link)
+            val shareIntent = Intent(Intent.ACTION_SEND)
+            shareIntent.setType("text/plain")
+            shareIntent.putExtra(Intent.EXTRA_TEXT, shareMessage)
+            startActivity(shareIntent)
+        }
+
+        //Кнопка Написать в поддержку
+        val settingsButtonSupport = findViewById<FrameLayout>(R.id.settings_button_support)
+        settingsButtonSupport.setOnClickListener {
+
+            val supportIntent = Intent(Intent.ACTION_SENDTO)
+            supportIntent.data = Uri.parse("mailto:")
+            supportIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf(getString(R.string.support_message_address)))
+            supportIntent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.support_message_theme))
+            supportIntent.putExtra(Intent.EXTRA_TEXT, getString(R.string.support_message_text))
+            startActivity(supportIntent)
+        }
+
+        //Кнопка Пользовательское соглашение
+        val settingsButtonAssignment = findViewById<FrameLayout>(R.id.settings_button_assignment)
+        settingsButtonAssignment.setOnClickListener {
+
+            val settingsIntent = Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.assignment_link)))
+            startActivity(settingsIntent)
+        }
+
+
     }
 }
