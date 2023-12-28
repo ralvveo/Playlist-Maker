b49b5121f18d3961241d2877cb57f9309ae6b23d
ralvveo <lvravdin@edu.hse.ru>
2023-12-28 21:35:28 +0300

    Спринт 10 попытка 2

--- app/src/main/java/com/practicum/playlistmaker/TracksAdapter.kt
+++ app/src/main/java/com/practicum/playlistmaker/TracksAdapter.kt
@@ -5,20 +5,20 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
 class TracksAdapter(
-    private val news: List<Track>
+    private val tracks: List<Track>
 ) : RecyclerView.Adapter<TracksViewHolder> () {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TracksViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.`track_item`, parent, false)
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.track_item, parent, false)
         return TracksViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: TracksViewHolder, position: Int) {
-        holder.bind(news[position])
+        holder.bind(tracks[position])
     }
 
     override fun getItemCount(): Int {
-        return news.size
+        return tracks.size
     }
 
 }
--- app/src/main/java/com/practicum/playlistmaker/TracksViewHolder.kt
+++ app/src/main/java/com/practicum/playlistmaker/TracksViewHolder.kt
@@ -10,7 +10,9 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import com.bumptech.glide.load.resource.bitmap.FitCenter
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.load.resource.bitmap.TransformationUtils.fitCenter
 
 class TracksViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
 
@@ -19,16 +21,14 @@ class TracksViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
     private val trackArtistName: TextView = itemView.findViewById(R.id.trackItemArtistName)
     private val trackTime: TextView = itemView.findViewById(R.id.trackItemTime)
 
-
-
-
     fun bind(model: Track) {
         trackName.text = model.trackName
         trackArtistName.text = model.artistName
         trackTime.text = model.trackTime
+        val roundedCornersSize: Int = 8
         Glide.with(itemView)
             .load(model.artworkUrl100)
-            .transform(CenterCrop(), RoundedCorners(8))
+            .transform(FitCenter(), RoundedCorners(roundedCornersSize))
             .placeholder(R.drawable.track_image_placeholder)
             .into(trackImage)
     }
