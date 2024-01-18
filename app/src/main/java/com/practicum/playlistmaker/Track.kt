2d95c8645bff11f639b73b382d226c5107875061
ralvveo <lvravdin@edu.hse.ru>
2024-01-18 13:46:34 +0300

    Спринт 13 попытка 1

--- app/src/main/java/com/practicum/playlistmaker/Track.kt
+++ app/src/main/java/com/practicum/playlistmaker/Track.kt
@@ -6,6 +6,10 @@ data class Track (
     val trackName: String,
     val artistName: String,
     @SerializedName("trackTimeMillis") val trackTime: Long,
-    val artworkUrl100: String
+    val artworkUrl100: String,
+    val collectionName: String,
+    val releaseDate: String,
+    val primaryGenreName: String,
+    val country: String
 
 )
--- app/src/main/java/com/practicum/playlistmaker/TracksAdapter.kt
+++ app/src/main/java/com/practicum/playlistmaker/TracksAdapter.kt
@@ -1,5 +1,6 @@
 package com.practicum.playlistmaker
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
@@ -18,10 +19,20 @@ class TracksAdapter(
 
     override fun onBindViewHolder(holder: TracksViewHolder, position: Int) {
         holder.bind(tracks[position])
-
+        val context = holder.itemView.context
         holder.itemView.setOnClickListener {
             searchHistory.addTrackToHistory(tracks[position])
-
+            val displayIntent = Intent(context, AudioplayerActivity::class.java)
+            displayIntent.putExtra("trackName", tracks[position].trackName)
+            displayIntent.putExtra("artistName", tracks[position].artistName)
+            displayIntent.putExtra("duration", tracks[position].trackTime)
+            displayIntent.putExtra("artworkUrl100", tracks[position].artworkUrl100)
+            displayIntent.putExtra("collectionName", tracks[position].collectionName)
+            displayIntent.putExtra("releaseDate", tracks[position].releaseDate)
+            displayIntent.putExtra("Genre", tracks[position].primaryGenreName)
+            displayIntent.putExtra("country", tracks[position].country)
+            context.startActivity(displayIntent)
+            //Log.d("qciuibcubicicbnicw;ib", "${tracks[position].trackName} ${tracks[position].artistName} ${tracks[position].trackTime} ${tracks[position].artworkUrl100} \n ${tracks[position].collectionName} \n ${tracks[position].releaseDate} \n ${tracks[position].primaryGenreName} \n ${tracks[position].country}")
         }
 
 
