d14221acfd3cba1ecc40f2ed9ed3e58fbd50fc75
ralvveo <lvravdin@edu.hse.ru>
2024-02-11 22:48:42 +0300

    Спринт 14 попытка 1

--- app/src/main/java/com/practicum/playlistmaker/AudioplayerActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/AudioplayerActivity.kt
@@ -1,6 +1,10 @@
 package com.practicum.playlistmaker
 
+import android.content.Intent
+import android.media.MediaPlayer
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.View
 import android.widget.ImageButton
 import android.widget.ImageView
@@ -10,13 +14,38 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.CenterCrop
 import com.bumptech.glide.load.resource.bitmap.FitCenter
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.practicum.playlistmaker.databinding.ActivityAudioplayerBinding
+import com.practicum.playlistmaker.databinding.ActivitySearchBinding
 import java.text.SimpleDateFormat
 import java.util.Locale
 
 class AudioplayerActivity() : AppCompatActivity() {
+
+    //Все состояния медиаплеера
+    companion object{
+        private const val STATE_DEFAULT = 0
+        private const val STATE_PREPARED = 1
+        private const val STATE_PLAYING = 2
+        private const val STATE_PAUSED = 3
+        private const val DELAY = 500L
+    }
+
+    private lateinit var binding: ActivityAudioplayerBinding
+
+    //Начальное состояние медиаплеера
+    private var playerState = STATE_DEFAULT
+    private var handler = Handler(Looper.getMainLooper())
+    private var mediaPlayer = MediaPlayer()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_audioplayer)
+        binding = ActivityAudioplayerBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        preparePlayer()
+
 
         //Get Track info
         val infoTrackName = intent.getStringExtra("trackName")
@@ -28,48 +57,130 @@ class AudioplayerActivity() : AppCompatActivity() {
         val infoTrackGenre = intent.getStringExtra("Genre")
         val infoTrackCountry = intent.getStringExtra("country")
 
-        //Initialize track Info
-        val trackName = findViewById<TextView>(R.id.track_name)
-        val trackArtistName = findViewById<TextView>(R.id.track_author)
-        val trackImage = findViewById<ImageView>(R.id.track_image)
-        val trackDuration = findViewById<TextView>(R.id.track_duration_text)
-        val trackAlbum = findViewById<TextView>(R.id.track_album_text)
-        val trackYear= findViewById<TextView>(R.id.track_year_text)
-        val trackGenre = findViewById<TextView>(R.id.track_genre_text)
-        val trackCountry = findViewById<TextView>(R.id.track_country_text)
 
-        val trackAlbumLeft = findViewById<TextView>(R.id.track_album)
 
-        trackName.text = infoTrackName
-        trackArtistName.text = infoTrackArtistName
-        trackDuration.text = SimpleDateFormat("mm:ss", Locale.getDefault()).format(infoTrackDuration)
-        trackAlbum.text = infoTrackCollectionName
-        trackYear.text = infoTrackReleaseDate?.take(4)
-        trackGenre.text = infoTrackGenre
-        trackCountry.text = infoTrackCountry
-
-        trackAlbumLeft.visibility = View.VISIBLE
-        trackAlbum.visibility = View.VISIBLE
+        binding.trackName.text = infoTrackName
+        binding.trackAuthor.text = infoTrackArtistName
+        binding.trackDurationText.text = SimpleDateFormat("mm:ss", Locale.getDefault()).format(infoTrackDuration)
+        binding.trackAlbumText.text = infoTrackCollectionName
+        binding.trackYearText.text = infoTrackReleaseDate?.take(4)
+        binding.trackGenreText.text = infoTrackGenre
+        binding.trackCountryText.text = infoTrackCountry
+        binding.trackAlbum.visibility = View.VISIBLE
+        binding.trackAlbumText.visibility = View.VISIBLE
 
 
         val infoTrackImage512 = infoTrackImage100?.replaceAfterLast('/',"512x512bb.jpg")
-        val roundedCornersSize: Int = 8
-        Glide.with(trackImage)
+        val roundedCornersSize = 8
+        Glide.with(binding.trackImage)
             .load(infoTrackImage512)
             .transform(RoundedCorners(roundedCornersSize))
             .placeholder(R.drawable.audioplayer_placeholder)
-            .into(trackImage)
+            .into(binding.trackImage)
 
         //Кнопка Назад
-        val audioplayerButtonBack = findViewById<ImageButton>(R.id.audioplayer_arrow_back)
-        audioplayerButtonBack.setOnClickListener {
+        binding.audioplayerArrowBack.setOnClickListener {
             finish()
+
         }
-        if (trackAlbum.text.isNullOrEmpty()){
-            trackAlbum.visibility = View.GONE
-            trackAlbumLeft.visibility = View.GONE
+
+        //Кнопка Воспроизведения
+        binding.audioplayerCenterButton.setOnClickListener{
+            playbackControl()
+        }
+
+
+
+        if (binding.trackAlbumText.text.isNullOrEmpty()){
+            binding.trackAlbumText.visibility = View.GONE
+            binding.trackAlbum.visibility = View.GONE
+        }
+
+
+
+
+
+    }
+
+    private fun startTimer(){
+        handler.post(createUpdateTimerTask())
+    }
+
+    private fun createUpdateTimerTask() : Runnable{
+        return object: Runnable{
+            override fun run() {
+                if (playerState == STATE_PLAYING){
+                    val elapsedTime = SimpleDateFormat("mm:ss", Locale.getDefault()).format(mediaPlayer.currentPosition)
+                    if (mediaPlayer.currentPosition < 30000){
+                        binding.trackCurrentTime.text = elapsedTime
+                        handler.postDelayed(this, DELAY)
+                    }
+                    else{
+                        binding.trackCurrentTime.text = "00:00"
+                    }
+                }
+            }
         }
     }
+
+    override fun onPause() {
+        super.onPause()
+        pausePlayer()
+    }
+
+    override fun onDestroy() {
+
+        super.onDestroy()
+        mediaPlayer.release()
+    }
+
+    private fun playbackControl() {
+        when(playerState) {
+            STATE_PLAYING -> {
+                pausePlayer()
+            }
+            STATE_PREPARED, STATE_PAUSED -> {
+                startPlayer()
+
+            }
+        }
+    }
+
+    private fun preparePlayer() {
+        val previewUrl = intent.getStringExtra("previewUrl")
+        mediaPlayer.setDataSource(previewUrl)
+        mediaPlayer.prepareAsync()
+        mediaPlayer.setOnPreparedListener {
+            binding.audioplayerCenterButton.isEnabled = true
+            playerState = STATE_PREPARED
+        }
+        mediaPlayer.setOnCompletionListener {
+            playerState = STATE_PREPARED
+            binding.trackCurrentTime.text = "00:00"
+            binding.audioplayerCenterButton.background = getDrawable(R.drawable.audioplayer_center_button_play)
+            handler.removeCallbacksAndMessages(createUpdateTimerTask())
+        }
+    }
+
+    private fun startPlayer() {
+        mediaPlayer.start()
+        startTimer()
+        playerState = STATE_PLAYING
+        binding.audioplayerCenterButton.background = getDrawable(R.drawable.audioplayer_center_button_pause)
+    }
+
+    private fun pausePlayer() {
+        mediaPlayer.pause()
+        handler.removeCallbacks(createUpdateTimerTask())
+        playerState = STATE_PAUSED
+        binding.audioplayerCenterButton.background = getDrawable(R.drawable.audioplayer_center_button_play)
+    }
+
+
+
+
+
+
 
 
 }
--- app/src/main/java/com/practicum/playlistmaker/TracksAdapter.kt
+++ app/src/main/java/com/practicum/playlistmaker/TracksAdapter.kt
@@ -1,16 +1,23 @@
 package com.practicum.playlistmaker
 
+
 import android.content.Intent
+import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.practicum.playlistmaker.SearchActivity.Companion.CLICK_DEBOUNCE_DELAY
 
 
 class TracksAdapter(
     private val tracks: List<Track>
 ) : RecyclerView.Adapter<TracksViewHolder> () {
 
-    val searchHistory = SearchHistory()
+    private var isClickAllowed = true
+
+    private val handler = Handler(Looper.getMainLooper())
+    private val searchHistory = SearchHistory()
 
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TracksViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.track_item, parent, false)
@@ -18,28 +25,42 @@ class TracksAdapter(
     }
 
     override fun onBindViewHolder(holder: TracksViewHolder, position: Int) {
-        holder.bind(tracks[position])
-        val context = holder.itemView.context
-        holder.itemView.setOnClickListener {
-            searchHistory.addTrackToHistory(tracks[position])
-            val displayIntent = Intent(context, AudioplayerActivity::class.java)
-            displayIntent.putExtra("trackName", tracks[position].trackName)
-            displayIntent.putExtra("artistName", tracks[position].artistName)
-            displayIntent.putExtra("duration", tracks[position].trackTime)
-            displayIntent.putExtra("artworkUrl100", tracks[position].artworkUrl100)
-            displayIntent.putExtra("collectionName", tracks[position].collectionName)
-            displayIntent.putExtra("releaseDate", tracks[position].releaseDate)
-            displayIntent.putExtra("Genre", tracks[position].primaryGenreName)
-            displayIntent.putExtra("country", tracks[position].country)
-            context.startActivity(displayIntent)
-            //Log.d("qciuibcubicicbnicw;ib", "${tracks[position].trackName} ${tracks[position].artistName} ${tracks[position].trackTime} ${tracks[position].artworkUrl100} \n ${tracks[position].collectionName} \n ${tracks[position].releaseDate} \n ${tracks[position].primaryGenreName} \n ${tracks[position].country}")
-        }
+
+            holder.bind(tracks[position])
+            val context = holder.itemView.context
+            holder.itemView.setOnClickListener {
+                if (clickDebounce()) {
+                    val displayIntent = Intent(context, AudioplayerActivity::class.java)
+                    displayIntent.putExtra("trackName", tracks[position].trackName)
+                    displayIntent.putExtra("artistName", tracks[position].artistName)
+                    displayIntent.putExtra("duration", tracks[position].trackTime)
+                    displayIntent.putExtra("artworkUrl100", tracks[position].artworkUrl100)
+                    displayIntent.putExtra("collectionName", tracks[position].collectionName)
+                    displayIntent.putExtra("releaseDate", tracks[position].releaseDate)
+                    displayIntent.putExtra("Genre", tracks[position].primaryGenreName)
+                    displayIntent.putExtra("country", tracks[position].country)
+                    displayIntent.putExtra("previewUrl", tracks[position].previewUrl)
+                    context.startActivity(displayIntent)
+                    searchHistory.addTrackToHistory(tracks[position])
+                }
+            }
+
+
 
 
     }
 
     override fun getItemCount(): Int {
         return tracks.size
+    }
+
+    private fun clickDebounce() : Boolean {
+        val current = isClickAllowed
+        if (isClickAllowed) {
+            isClickAllowed = false
+            handler.postDelayed({ isClickAllowed = true }, CLICK_DEBOUNCE_DELAY)
+        }
+        return current
     }
 
 }
